33ce618ae26388fd864342b1567305e8bd24d26d
AlveeGit <alveekabir1@gmail.com>
2023-07-18 16:56:58 +0600

    about, experience block added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,8 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px]  max-w-7xl mx-auto  flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -17,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>X</span>
+            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop Custom Websites<br className='sm:block hidden' />
-            and Web Applications
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
           </p>
         </div>
       </div>
@@ -43,10 +42,8 @@ const Hero = () => {
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
-          
         </a>
       </div>
-
     </section>
   );
 };
